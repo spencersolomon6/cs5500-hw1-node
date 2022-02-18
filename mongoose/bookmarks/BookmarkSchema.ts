10ec5102ef1fe9543ef14e4d96c1b4bab03bfebd
SpencerSolomon <spencersolomon6@gmail.com>
2022-02-18 15:41:55 -0500

    fixed issue in BookmarkSchema

--- mongoose/bookmarks/BookmarkSchema.ts
+++ mongoose/bookmarks/BookmarkSchema.ts
@@ -12,7 +12,7 @@
   * @property {Tuit} bookmarkedTuit Tuit being bookmarked
   */
  const BookmarkSchema = new mongoose.Schema<Bookmark>({
-    bookmarkedBy: {type: Schema.Types.ObjectId, ref: "TuitModel"},
-    bookmarkedTuit: {type: Schema.Types.ObjectId, ref: "UserModel"},
+    bookmarkedBy: {type: Schema.Types.ObjectId, ref: "UserModel"},
+    bookmarkedTuit: {type: Schema.Types.ObjectId, ref: "TuitModel"},
  }, {collection: "bookmarks"});
  export default BookmarkSchema;
